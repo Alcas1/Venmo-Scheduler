5412fd1e947df7144a4727c1be3464b68771bd90
Jonathan Wu <jonathanwu70@gmail.com>
2023-01-05 04:06:29 -0500

    add default header syntax

--- internal/clients/venmo/venmo.go
+++ internal/clients/venmo/venmo.go
@@ -1,6 +1,9 @@
 package clients
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 type (
 	Client interface {
@@ -11,11 +14,23 @@ type (
 	Service struct {
 		httpClient *http.Client
 	}
+
+	transport struct {
+		underlyingTransport http.RoundTripper
+	}
 )
+
+func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Add("x-api-key", "my-secret-token")
+	return t.underlyingTransport.RoundTrip(req)
+}
 
 func New() Service {
 	return Service{
-		httpClient: &http.Client{},
+		httpClient: &http.Client{
+			Timeout:   time.Second * 10,
+			Transport: &transport{underlyingTransport: http.DefaultTransport},
+		},
 	}
 }
 
