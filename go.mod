edcc72b22d1b5341a152b205ea202bdccd2213d6
Jonathan Wu <jonathanwu70@gmail.com>
2023-01-05 03:20:20 -0500

    add basic go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Alcas1/Venmo-Scheduler
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// make an api call to venmo api in go
+	response, err := http.Get("https://api.venmo.com/v1/payments?access_token=123456")
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s	", err)
+	}
+}
