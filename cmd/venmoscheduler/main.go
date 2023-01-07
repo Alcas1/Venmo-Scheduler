9e9dbdb8812f340325d6bacb9f64c034befe9f8a
Jonathan Wu <jonathanwu70@gmail.com>
2023-01-07 03:49:25 -0500

    use client to query access token

--- cmd/venmoscheduler/main.go
+++ cmd/venmoscheduler/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+
+	venmo "github.com/Alcas1/Venmo-Scheduler/internal/clients/venmo"
 )
 
 func main() {
+	venmoClient := venmo.New()
+	accessToken, err := venmoClient.GetAccessToken("username", "password")
+	fmt.Println(accessToken, err)
 	// make an api call to venmo api in go
-	response, err := http.Get("https://api.venmo.com/v1/payments?access_token=123456")
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s	", err)
-	}
+	// response, err := http.Get("https://api.venmo.com/v1/payments?access_token=123456")
+	// if err != nil {
+	// 	fmt.Printf("The HTTP request failed with error %s	", err)
+	// }
 
-	fmt.Println(response)
+	// fmt.Println(response)
 }
